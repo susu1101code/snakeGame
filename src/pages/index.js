9b07a708a9ae6fd459bae5a951ddf25a91b63d36
susu <715182333@qq.com>
2024-07-21 14:09:23 +1000

    snake game demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,184 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  //TODO 环境变量用不了，css怎么读环境变量
+  // process.env.
+  // 可选升级
+  //    增加食物种类以及功能
+  //    增加分数
+  //    增加蛇头尾的碰撞检测
+  //    优化外观
+  //    优化性能减少延迟
+
+  const INIT_DIRECT = "Right";
+  const ROW_LENGTH = 10;
+  const COL_LENGTH = 10;
+  const row = Array.apply(null, Array(ROW_LENGTH));
+  const column = Array.apply(null, Array(COL_LENGTH));
+
+  const [snakeList, setSnakeList] = useState([]);
+  const [foodPosition, setFoodPosition] = useState(0);
+  const [snakeDirection, setSnakeDirection] = useState(INIT_DIRECT);
+  const [walls, setWalls] = useState([]);
+  const [speed, setSpeed] = useState(300);
+  const [gameStart, setGameStart] = useState(false);
+
+  function wallsGenerator() {
+    const wallsPos = [];
+    for (let i = 0; i < ROW_LENGTH; i++) {
+      for (let j = 0; j < COL_LENGTH; j++) {
+        if (
+          i === 0 ||
+          i === ROW_LENGTH - 1 ||
+          j === 0 ||
+          j === COL_LENGTH - 1
+        ) {
+          wallsPos.push(getPositionFromRowIndexAndColIndex(i, j));
+        }
+      }
+    }
+    setWalls(wallsPos);
+    return wallsPos;
+  }
+  function getPositionFromRowIndexAndColIndex(rowIndex, colIndex) {
+    return rowIndex * ROW_LENGTH + colIndex;
+  }
+
+  function randomPosition(min = 0, max = 99) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function isValidTurn(prevDirection, direction) {
+    const directions = {
+      Up: ["Left", "Right"],
+      Down: ["Left", "Right"],
+      Left: ["Up", "Down"],
+      Right: ["Up", "Down"],
+    };
+
+    return directions[prevDirection].includes(direction);
+  }
+  function snakeTurnHandler(event) {
+    const direction = {
+      ArrowUp: "Up",
+      ArrowDown: "Down",
+      ArrowLeft: "Left",
+      ArrowRight: "Right",
+    }[event.key];
+    if (direction && isValidTurn(snakeDirection, direction)) {
+      setSnakeDirection(direction);
+    }
+  }
+  function gameInit() {
+    const w = wallsGenerator();
+    const s = snakeGenerator();
+    foodGenerator(w, s);
+    setGameStart(true);
+  }
+  function gameStartHandler() {
+    gameInit();
+  }
+  useEffect(() => {
+    const w = wallsGenerator();
+  }, []);
+
+  //TODO
+  // 有延迟
+  useEffect(() => {
+    const interval = setInterval(() => {
+      snakeMove(interval);
+    }, speed);
+    window.addEventListener("keydown", snakeTurnHandler);
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener("keydown", snakeTurnHandler);
+    };
+  }, [snakeList, gameStart]);
+
+  function foodGenerator(w, s) {
+    let position = randomPosition();
+    while (s.includes(position) || w.includes(position)) {
+      position = randomPosition();
+    }
+    setFoodPosition(position);
+  }
+
+  //TODO longer gameInit snake
+  // in random position
+  function snakeGenerator(length = 1) {
+    setSnakeList([44]);
+    return [44];
+  }
+
+  function snakeMove(interval) {
+    if (!gameStart) {
+      clearInterval(interval);
+      return;
+    }
+    var newPosition;
+    switch (snakeDirection) {
+      case "Up":
+        newPosition = snakeList[0] - COL_LENGTH;
+        break;
+      case "Down":
+        newPosition = snakeList[0] + ROW_LENGTH;
+        break;
+      case "Left":
+        newPosition = snakeList[0] - 1;
+        break;
+      case "Right":
+        newPosition = snakeList[0] + 1;
+        break;
+      default:
+      // code block
+    }
+    if (gameOverCheck(newPosition)) {
+      return;
+    }
+    if (newPosition === foodPosition) {
+      setSnakeList([newPosition, ...snakeList]);
+      foodGenerator(walls, snakeList);
+    } else {
+      setSnakeList([newPosition, ...snakeList.slice(0, -1)]);
+    }
+  }
+  //TODO
+  // snake stuck by itself
+  function gameOverCheck(position) {
+    if (walls.includes(position)) {
+      alert("gameOver");
+      setGameStart(false);
+      return true;
+    }
+    return false;
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className={"body-container"}>
+      <section className="board-grid__container">
+        {row.map((_, rowIndex) => {
+          return column.map((_, colIndex) => {
+            let curPos = getPositionFromRowIndexAndColIndex(rowIndex, colIndex);
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <div
+                className={[
+                  "grid-item",
+                  walls.includes(curPos) ? "wall" : null,
+                  foodPosition === curPos ? "food" : null,
+                  snakeList.includes(curPos) ? "snake" : null,
+                ].join(" ")}
+              ></div>
+            );
+          });
+        })}
+      </section>
+      <button onClick={gameStartHandler}>start</button>
+    </div>
   );
 }
